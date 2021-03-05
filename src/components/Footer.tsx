c7b1bcf8d51c4eef10ee4add399973bec6eab1db
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2021-03-05 19:55:25 +0100

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
 import {FC} from 'react';
 
+import style from './Footer.module.scss';
 import {ResponsiveContainer} from './ResponsiveContainer';
 
 export const Footer: FC = ({children}) => (
-    <footer>
+    <footer className={style.main}>
         <ResponsiveContainer>
+            <div className={style.center}>
             {children}
+            </div>
         </ResponsiveContainer>
     </footer>
 );
