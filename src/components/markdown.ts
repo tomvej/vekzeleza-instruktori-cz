05ad258c195204b682f00e8d3fde481457274c8d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2021-03-15 22:37:39 +0100

    markdown helper functions

--- src/components/markdown.ts
+++ src/components/markdown.ts
@@ -1,7 +1,15 @@
 import {createElement, Fragment, ReactElement} from 'react';
 import rehype from 'rehype-react';
 
-export type MarkdownRenderer = (ast: GatsbyTypes.Scalars['JSON']) => ReactElement;
+type Ast = GatsbyTypes.Scalars['JSON'];
+
+interface Node {
+    type: string;
+    tagName?: string;
+    children: Node[]
+}
+
+export type MarkdownRenderer = (ast: Ast | Node) => ReactElement;
 
 // @ts-expect-error apparently using rehype internals which are not typed
 export const createMarkdownRenderer = (components = {}): MarkdownRenderer => new rehype({
@@ -11,3 +19,18 @@ export const createMarkdownRenderer = (components = {}): MarkdownRenderer => new
 }).Compiler;
 
 export const renderMarkdown = createMarkdownRenderer();
+
+export const renderNodes = (renderer: MarkdownRenderer, children: Node[]): ReactElement => renderer({type: 'root', children});
+
+
+const isHeading = ({type, tagName}: Node) => type === 'element' && tagName === 'h1';
+
+export const splitHeading = (ast: Ast): {heading?: Node, nodes: Node[]} => {
+    // @ts-expect-error
+    const childNodes: Node[] = ast.children;
+
+    const heading: Node | undefined = childNodes.find(isHeading);
+    const nodes: Node[] = childNodes.filter((node) => !isHeading(node));
+
+    return {heading, nodes};
+};
